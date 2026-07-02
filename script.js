c23ac540426f793b51ce79204305f6ada7e14bf1
M9580 <ackermanmikasu@gmail.com>
2026-07-02 11:08:47 +0530

    Add password check function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function checkPassword() {
 
+let pass = document.getElementById("password").value;
+
+if(pass === "1907"){
+
+document.getElementById("password-screen").style.display="none";
+
+document.getElementById("main-content").style.display="block";
+
+}else{
+
+document.getElementById("wrong").innerHTML="❌ Wrong Password! Try Again.";
+
+}
+
+}
